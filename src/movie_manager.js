69305f7569171976d3f24b2dfb856034eacbf5f8
Anna W <tester.kodilla@gmail.com>
2021-11-15 23:21:11 +0000

    Tryin to allocate seats in full theatre, passing test

--- src/movie_manager.js
+++ src/movie_manager.js
@@ -4,8 +4,12 @@ const allocateSeats = (seats, noOfSeats) => {
     if (!isValidSeats(seats)) throw new Error("seats not valid");
     if (!isValidNoOfSeats(noOfSeats)) throw new Error("noOfSeats not valid");
 
-    let allocatedSeats = "";
     const firstAvailable = seats.findIndex(seat => seat !== "allocated");
+    if (firstAvailable === -1 || firstAvailable + noOfSeats > 15) {
+        console.log("Not enough seats");
+        return "";
+    }
+    let allocatedSeats = "";
     for (let i = firstAvailable; i < firstAvailable + noOfSeats; i++) {
         if (i !== firstAvailable) allocatedSeats += ", ";
         allocatedSeats += seats[i];
