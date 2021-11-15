b5b98a9af9784360862e589d856204fdea21e220
Anna W <tester.kodilla@gmail.com>
2021-11-15 22:04:34 +0000

    Some more passing tests

--- test/movie_manager.test.js
+++ test/movie_manager.test.js
@@ -14,9 +14,23 @@ test.each([
     [3, "B5, C1, C2"],
     [2, "C3, C4"],
     [1, "C5"],
-])("returns allocated seats", (seatsRequested, expected) => {
+])("returns allocated seats: seats requested %d, allocated %s", (seatsRequested, expected) => {
     // Act
     const result = allocateSeats(seats, seatsRequested);
+    // Assert
+    expect(result).toBe(expected);
+});
+
+test.each([
+    // Arrange
+    [2, "A1, A2"],
+    [3, "A1, A2, A3"],
+    [1, "A1"],
+])("returns allocated seats: seats requested %d, allocated %s", (seatsRequested, expected) => {
+    // Arrange
+    const seats2 = initialise();
+    // Act
+    const result = allocateSeats(seats2, seatsRequested);
     // Assert
     expect(result).toBe(expected);
 });
