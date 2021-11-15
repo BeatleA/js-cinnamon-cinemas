998ac839acdf8e2765c667a2660805fc595359d3
Anna W <tester.kodilla@gmail.com>
2021-11-15 21:49:56 +0000

    Second test for allocateSeats function - passing

--- src/movie_manager.js
+++ src/movie_manager.js
@@ -1,5 +1,12 @@
 const allocateSeats = (seats, seatsRequested) => {
-    return "A1";
+    let allocatedSeats = "";
+    const firstAvailable = seats.findIndex(seat => seat !== "allocated");
+    for (let i = firstAvailable; i < firstAvailable + seatsRequested; i++) {
+        if (i !== firstAvailable) allocatedSeats += ", ";
+        allocatedSeats += seats[i];
+        seats[i] = "allocated";
+    }
+    return allocatedSeats;
 }
 
 module.exports = allocateSeats;
