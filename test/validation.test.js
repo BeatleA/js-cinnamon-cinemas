4c5c08cc514e4425ce9aa8c170e0c2ad545641b1
Anna W <tester.kodilla@gmail.com>
2021-11-15 22:24:13 +0000

    Passing test of isValidNoOfSeats

--- test/validation.test.js
+++ test/validation.test.js
@@ -5,9 +5,11 @@ test.each([
     [1, true],
     [2, true],
     [3, true],
-    [0, false]
+    [0, false],
+    [4, false]
 ])("when requesting %d seats returns %p", (requestedSeats, expected) => {
     // Act
     const result = isValidNoOfSeats(requestedSeats);
+    // Assert
     expect(result).toBe(expected);
 });
