5657bc35176a60de811a8e92765f8d1ffd4de0ca
Anna W <tester.kodilla@gmail.com>
2021-11-15 23:35:46 +0000

    Validating whether noOfSeats is an Integer, passing test

--- src/validation.js
+++ src/validation.js
@@ -1,4 +1,4 @@
-const isValidNoOfSeats = noOfSeats => noOfSeats > 0 && noOfSeats < 4;
+const isValidNoOfSeats = noOfSeats => Number.isInteger(noOfSeats) && noOfSeats > 0 && noOfSeats < 4;
 
 const isValidSeats = seats => Array.isArray(seats) && seats.length === 15;
 
