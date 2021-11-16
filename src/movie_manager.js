ba79f5bd8c517251ef849a960988d2817cb92848
Anna W <tester.kodilla@gmail.com>
2021-11-16 10:00:15 +0000

    Refactored allocateSeats function

--- src/movie_manager.js
+++ src/movie_manager.js
@@ -4,18 +4,18 @@ const allocateSeats = (seats, noOfSeats) => {
     if (!isValidSeats(seats)) throw new Error("seats not valid");
     if (!isValidNoOfSeats(noOfSeats)) throw new Error("noOfSeats not valid");
 
-    const firstAvailable = seats.findIndex(seat => seat !== "allocated");
-    if (firstAvailable === -1 || firstAvailable + noOfSeats > 15) {
-        console.log("Not enough seats");
-        return "";
-    }
     let allocatedSeats = "";
-    for (let i = firstAvailable; i < firstAvailable + noOfSeats; i++) {
-        if (i !== firstAvailable) allocatedSeats += ", ";
-        allocatedSeats += seats[i];
-        seats[i] = "allocated";
+    const firstAvailable = findAvailableSeat(seats);
+    if (hasEnoughSeats(firstAvailable, noOfSeats)) {
+        allocatedSeats += seats.splice(firstAvailable, noOfSeats, ...Array(noOfSeats).fill("allocated")).join(", ");
+    } else {
+        console.log("Not enough seats");
     }
     return allocatedSeats;
 }
+
+const findAvailableSeat = seats => seats.findIndex(seat => seat !== "allocated");
+
+const hasEnoughSeats = (firstAvailable, noOfSeats) => firstAvailable !== -1 && firstAvailable + noOfSeats <= 15;
 
 module.exports = allocateSeats;
